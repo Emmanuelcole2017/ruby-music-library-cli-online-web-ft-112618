94da3fa524eedab00921d16730cf846df0ed7359
Emmanuel Cole <emmanuel.c2011@gmail.com>
2019-01-17 00:32:23 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -46,8 +46,7 @@ class MusicLibraryController
     
     # Iterate through sorted songs
     songNames.each_with_index{|songName, index|
-      puts "#{index + 1}. 
-      #{Song.find_by_name(songName).artist.name}"
+      puts "#{index + 1}. #{Song.find_by_name(songName).artist.name} - #{songName} - #{Song.find_by_name(songName).genre.name}"
     }
   end
     
