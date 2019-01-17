9cc55edd5a25a3e2d33a7570490bd872c9175fa6
Emmanuel Cole <emmanuel.c2011@gmail.com>
2019-01-17 01:02:06 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -63,9 +63,10 @@ class MusicLibraryController
     # Sort Artist names
     artistNames.sort!
     
+    puts Artist.all
     # Iterate through artistNames and output them
-    artistNames.each_with_index{|artistName, index|
-      puts "#{index + 1}. #{artistName}"
-    }
+    #artistNames.each_with_index{|artistName, index|
+    #  puts "#{index + 1}. #{artistName}"
+    #}
   end
 end
